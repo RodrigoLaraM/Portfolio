ccccca1fa3175fe0f55b0a8ca483f2cda65349a0
Rodrigo Lara <rodrigo.laram@outlook.com>
2022-07-13 17:56:51 -0500

    Change

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,6 @@
-cd Portfolio
 git fetch && git reset origin/main --hard
 virtualenv env
-tmux kill-session
 source env/bin/activate
-pip install -r requirements.txt
 systemctl restart myportfolio
 source python3-virtualenv/bin/activate
 flask run --host=0.0.0.0
---
+++ tmuxset.sh
@@ -0,0 +1,2 @@
+tmux kill-session
+tmux new
