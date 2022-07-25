12c41f9b50bd48f85c74b49b056c86269558fa68
Rodrigo Lara <rodrigo.laram@outlook.com>
2022-07-25 16:37:15 -0500

    Adding Tests

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,18 @@ from peewee import *
 from playhouse.shortcuts import model_to_dict
 import datetime as dt
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-user=os.getenv("MYSQL_USER"),
-password=os.getenv("MYSQL_PASSWORD"),
-host=os.getenv("MYSQL_HOST"),
-port=3306)
-
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    # MySQL setup with peewee
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            host=os.getenv("MYSQL_HOST"),
+            port=3306
+    ) 
+    
 print(mydb)
 
 class TimelinePost(Model):
--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,5 @@
 git fetch && git reset origin/main --hard
 virtualenv env
-source env/bin/activate
 systemctl restart myportfolio
 source python3-virtualenv/bin/activate
 flask run --host=0.0.0.0
